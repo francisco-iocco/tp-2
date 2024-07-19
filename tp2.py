e3f023329a2dee101889668a09f1ffef02e29306
mLumas <lucasmichelini1846@gmail.com>
2024-07-18 21:31:04 -0300

    login de email y contra hecho, errores solucionados

--- tp2.py
+++ tp2.py
@@ -1,7 +1,11 @@
 '''Lucas Michelini, Francisco Iocco, Sebastian Gonzales, Joaquin Benitez'''
 import random
-
+import os
 matriz = [[random.randint(0,1) for _ in range(7)] for _ in range(7)]
+def clearConsole():
+    if os.name in ('nt', 'dos'):  
+        command = 'cls'
+    os.system(command)
 estudiantesIngreso = [["Lucasmichelini@gmail.com","123","ACTIVO"],
                        ["franciscoiocco@gmail.com","123","ACTIVO"],
                        ["sebastiangonzales@gmail.com","123","ACTIVO"],
@@ -41,8 +45,9 @@ def menuEstudiantes(opcion, subOpcion):
         case _:
             print("Por favor, elija una opcion válida.")
     
-def Logueo(cEA,cMA,inicio):
+def Inicializacion(cEA,cMA,inicio):
     while(inicio != "LOGIN" and inicio != "REGISTRARSE"):
+        clearConsole()
         print("Opcion no valida\nporfavor ingrese una de las siguientes opciones")
         inicio = input("-LOGIN\n-REGISTRARSE\nIngrese su opcion:")
         inicio = inicio.upper() 
@@ -55,38 +60,56 @@ def Logueo(cEA,cMA,inicio):
 inicio = input("-LOGIN\n-REGISTRARSE\nIngrese su opcion:")
 inicio = inicio.upper()
 
-while(not(Logueo(cantEstudiantesActivos,cantModeradoresActivos,inicio))):
+while(not(Inicializacion(cantEstudiantesActivos,cantModeradoresActivos,inicio))):
+    clearConsole()
     print("Necesita ingresar mas estudiantes[",cantEstudiantesActivos,"/4] o modereadores[",cantModeradoresActivos,"/1]")
     inicio = input("-LOGIN\n-REGISTRARSE\nIngrese su opcion:")
     inicio = inicio.upper()
     #HACER UNA FUNCION PARA REGISTRAR ESTUDIANTES'''
 
-while(inicio == "REGISTRARSE" and cantEstudiantesActivos < 8 or cantModeradoresActivos < 4):
+while(inicio == "REGISTRARSE"):
+    clearConsole()
     if(cantEstudiantesActivos < 8 or cantModeradoresActivos < 4 ):
         #HACER UNA FUNCION PARA REGISTRAR ESTUDIANTES'''
         inicio = input("-LOGIN\n-REGISTRARSE\nIngrese su opcion:")
         inicio = inicio.upper()
          
-        
-email = input("Ingrese su email: ")
-contrasena = input("ingrese su contraseña: ")
 
+def login(email,contrasena,baseDePerfiles):
+    perfilEncontrado = 0
+    encontrado = False 
+    for i in range(len(baseDePerfiles)):
+        if(email == baseDePerfiles[i][0] and
+        contrasena == baseDePerfiles[i][1] and
+        baseDePerfiles[i][2] == "ACTIVO"):
+            encontrado = True
+            perfilEncontrado = i
+    if(encontrado):
+        return perfilEncontrado
+    else:
+        return -1
+            
 estudianteActual = -1
 moderadorActual = -1
-
+clearConsole()        
+email = input("Ingrese su email: ")
+contrasena = input("ingrese su contraseña: ")
 contIntentos = 0
-while(contIntentos < 3):            
-    for i in range(len(estudiantesIngreso)):
-        if(email == estudiantesIngreso[i][0] and
-        contrasena == estudiantesIngreso[i][1] and
-        estudiantesIngreso[i][2] == "ACTIVO"):
-            estudianteActual = i
-            moderadorActual = -1
-        elif(email == moderadorIngreso[i][0] and        #PROBLEMA AL TRATAR DE LEER MODERADOR EN POSICION
-            contrasena == moderadorIngreso[i][1] and    #MAYOR Q 4(SIN SOLUCIONAR)
-            moderadorIngreso[i][2] == "ACTIVO"):
-            moderadorActual = i
-            estudianteActual = -1
-        else:
-            contIntentos+=1
-print(estudianteActual, moderadorActual)            
+while(contIntentos < 3 and estudianteActual == -1 and moderadorActual == -1):            
+    estudianteActual = login(email,contrasena,estudiantesIngreso)
+    moderadorActual = login(email,contrasena,moderadorIngreso)
+    
+    if(estudianteActual == -1 and moderadorActual == -1):
+        contIntentos += 1
+        clearConsole()
+        email = input("Ingrese su email: ")
+        contrasena = input("ingrese su contraseña: ")
+        
+
+if(estudianteActual != -1):
+    print(estudianteActual)
+elif(moderadorActual != -1):
+    print(moderadorActual)
+else:
+    print("n")
+
